caf925ee006051cab344085f5f0bf956e3b783d3
youssef-1998 <bouzar26@gmail.com>
2019-11-19 19:03:11 +0100

    Updates

---
+++ precache-manifest.ee97e786dfead048489d4bb74f7199f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "575b54c4d885e99af6c4471007307328",
+    "url": "/REACT-TODO-APP/index.html"
+  },
+  {
+    "revision": "adf425159b42554658ed",
+    "url": "/REACT-TODO-APP/static/css/main.804368fa.chunk.css"
+  },
+  {
+    "revision": "f521418c554c19807602",
+    "url": "/REACT-TODO-APP/static/js/2.0c6fc3b6.chunk.js"
+  },
+  {
+    "revision": "adf425159b42554658ed",
+    "url": "/REACT-TODO-APP/static/js/main.7c7a8158.chunk.js"
+  },
+  {
+    "revision": "1bf20ab82487e08a1c61",
+    "url": "/REACT-TODO-APP/static/js/runtime-main.476b43f4.js"
+  },
+  {
+    "revision": "68a5ccd7471aa49c7ec6ca7fb7ce39b7",
+    "url": "/REACT-TODO-APP/static/media/logo.68a5ccd7.png"
+  }
+]);
